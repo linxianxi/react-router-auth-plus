a455c232f78b889b54f805c5fecddf00c7b5ee17
linxianxi <904492381@qq.com>
2022-07-22 11:57:21 +0800

    fix: cjs build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "./lib/index.ts"),
+      entry: "lib/index.ts",
       name: "reactAuthRouter",
       fileName: "index",
       formats: ["es", "cjs", "umd"],
