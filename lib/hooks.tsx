371fc1e2e00cadede91dd37c916d356215f295c3
linxianxi <904492381@qq.com>
2022-07-25 15:22:23 +0800

    feat: add getAuthMenus util、change auth type

--- lib/hooks.tsx
+++ lib/hooks.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RouteObject, useRoutes } from "react-router-dom";
 import { AuthLayout, NoAuthElement } from "./components";
+import cloneDeep from "lodash.clonedeep";
 
 export interface AuthRouterObject extends RouteObject {
   auth?: string | string[];
@@ -15,13 +16,12 @@ export const useAuthRouters = ({
 }: {
   routers: AuthRouterObject[];
   noAuthElement?: (router: AuthRouterObject) => React.ReactNode;
-  auth: string | string[];
+  auth: string[];
   render?: (element: React.ReactElement | null) => React.ReactElement | null;
 }) => {
   const getRouters = (routers: AuthRouterObject[]) => {
     const result: AuthRouterObject[] = [];
-    routers.forEach((_router) => {
-      const router = { ..._router };
+    cloneDeep(routers).forEach((router) => {
       if (router.auth) {
         const setNoAuthElement = () => {
           router.element = (
@@ -35,30 +35,19 @@ export const useAuthRouters = ({
           );
         };
         if (typeof router.auth === "string") {
-          if (typeof auth === "string") {
-            if (auth !== router.auth) {
-              setNoAuthElement();
-            }
-          } else {
-            if (!auth.includes(router.auth)) {
-              setNoAuthElement();
-            }
+          if (!auth.includes(router.auth)) {
+            setNoAuthElement();
           }
         } else if (Array.isArray(router.auth) && router.auth.length > 0) {
-          if (typeof auth === "string") {
-            if (!router.auth.includes(auth)) {
-              setNoAuthElement();
-            }
-          } else {
-            if (!router.auth.some((i) => auth.includes(i))) {
-              setNoAuthElement();
-            }
+          if (!router.auth.some((i) => auth.includes(i))) {
+            setNoAuthElement();
           }
         }
       }
       if (React.isValidElement(router.element) && router.children) {
         router.element = React.cloneElement(router.element, {
           routers: router.children,
+          ...router.element.props,
         });
       }
       if (router.children) {
---
+++ lib/util.ts
@@ -0,0 +1,31 @@
+import cloneDeep from "lodash.clonedeep";
+import { AuthRouterObject } from "./hooks";
+
+export function getAuthMenus<T extends AuthRouterObject>(
+  auth: string[],
+  menuRouters: T[]
+): T[] {
+  const result: T[] = [];
+
+  cloneDeep(menuRouters).forEach((i) => {
+    if (!i.auth || (Array.isArray(i.auth) && !i.auth.length)) {
+      result.push(i);
+    } else {
+      if (typeof i.auth === "string") {
+        if (auth.includes(i.auth)) {
+          result.push(i);
+        }
+        return false;
+      } else {
+        if (auth.some((item) => i.auth?.includes(item))) {
+          result.push(i);
+        }
+      }
+    }
+    if (i.children?.length) {
+      i.children = getAuthMenus(auth, i.children);
+    }
+  });
+
+  return result;
+}
